79410fdbe68a3a0b5b84670a929551668a14ca69
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-06-05 22:18:10 +0700

    input can now be read

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return request.form["input"]
 
 
 if __name__ == "__main__" :
-    app.run(host="127.0.0.1", port=5000)
+    app.run(debug = True, host="127.0.0.1", port=5000)
