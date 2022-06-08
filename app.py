585b2db3653fc0500110e5e7f8aa2abf8d534442
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-06-08 22:00:45 +0700

    edited app.run()

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def grammar():
     return jsonify(data)
 
 if __name__ == "__main__" :
-    app.run(debug = True, host="127.0.0.1", port=5000)
+    app.run(debug = False, host="192.168.1.7", port=9999)
